94859c2b7807b7d71b21f4cd16040ed2a2d39d1d
Mirlan Ipasov <mipasov@gmail.com>
2020-03-16 13:04:39 +0600

    spring boot app initialization

---
+++ src/main/java/com/devxschool/foodhouse/FoodhouseApplication.java
@@ -0,0 +1,13 @@
+package com.devxschool.foodhouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodhouseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodhouseApplication.class, args);
+	}
+
+}
